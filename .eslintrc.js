a53263539371ce0c5bb1c8ddcb85bbb6581d6dad
Andy Pak <skandla2002@gmail.com>
2021-09-01 22:20:57 +0900

    Link를 통한 Route 처리

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,15 @@ module.exports = {
     "spaced-comment": "off", // 주석을 뒤에쓰지 말라는 경고
     "@typescript-eslint/no-unused-vars": "warn", // 사용 안 한 변수는 경고
     "jsx-a11y/control-has-associated-label": "off", // 상호 작용하는 엘리먼트에 label을 넣는다.
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ], // Link안에 <a> tag 있는 경우 에러 개선
+    //: a11y/blob/master/docs/rules/anchor-is-valid.mdeslintjsx-a11y/anchor-is-valid
     "react/no-array-index-key": "off", // key값으로 index를 사용 할 수있다.
     "comma-dangle": "off", // 마지막에 ,을 넣어주지 않는다.
     "arrow-body-style": "off", // 화살표 함수내에 return을 사용 할 수 있다.
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,15 @@
-// eslint-disable-next-line react/react-in-jsx-scope
-const App = () => <div>Hello Next.js!!</div>;
+import React from "react";
+import Link from "next/link";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Link to 'tomato' Page</h2>
+      <Link href="/tomato">
+        <a>Move to '/tomato'</a>
+      </Link>
+    </div>
+  );
+};
+
 export default App;
