ee7d43bbcfa1c089167ac061d5eb77236f13bc29
Andy Pak <skandla2002@gmail.com>
2021-09-02 23:17:22 +0900

    router 객체 보기

--- pages/vegetable/[name].jsx
+++ pages/vegetable/[name].jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const name = () => (
-  <div>
-    <h2>Hello!!</h2>
-    <Link href="/">Move to "/"</Link>
-  </div>
-);
+const name = () => {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <div>
+      <h2>Hello!!</h2>
+      <Link href="/">Move to '/'</Link>
+    </div>
+  );
+};
 export default name;
