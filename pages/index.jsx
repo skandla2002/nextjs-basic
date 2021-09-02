45b7ce582d49aa3dfa3aab1480aa44ccc65331ec
Andy Pak <skandla2002@gmail.com>
2021-09-02 23:13:08 +0900

    관리자 모드로 실행

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Link from "next/link";
 
+const Child = () => {
+  return <a>Move to '/tomato11'</a>;
+};
+// const App = () => {
+//   return (
+//     <div>
+//       <h2>Link to 'tomato11111' Page</h2>
+//       {/* <Link href="/tomato">
+//         <Child /> */}
+//       {/* <a>Move to '/tomato'</a> */}
+//       {/* <p>Move to '/tomato'</p> */}
+//       {/* </Link> */}
+//       <Link href="/vegetable/potato">
+//         <a>Move to '/vegetable/potato'</a>
+//       </Link>
+//     </div>
+//   );
+// };
+
 const App = () => {
   return (
     <div>
-      <h2>Link to 'tomato' Page</h2>
-      <Link href="/tomato">
-        <a>Move to '/tomato'</a>
+      <h2>Link to 'potato11' Page</h2>
+      <Link href="/vegetable/potato">
+        <a>Move to '/vegetable/potato'</a>
       </Link>
     </div>
   );
